5e6f7ba89acf39794c577aead278f35f722c89c6
Piotr Bielak <piotrbielak2@gmail.com>
2019-01-16 12:02:43 +0100

    Updated tests.sh script

--- tests.sh
+++ tests.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+# Args: data num-actors num-chunks
+
 set -e
 
-if [ ! -f "./target/release/local-sorting-actors" ]; then
+if [ ! -f "./target/release/supervisor" ] && [ ! -f "./target/release/sorting_actor" ]; then
     echo "Project not built!"
     cargo build --release
 fi
 
-for n in $1; do
-  echo "N = $n"
-  for r in $(seq 1 $2); do
-    ./target/release/local-sorting-actors -i $3 -o - -n $n -k $n;
-  done
+for n in $(seq 1 $2); do
+    ./target/release/sorting_actor --supervisor-addr 127.0.0.1:8000 1>/dev/null 2>&1 &
 done
+
+./target/release/supervisor --addr 127.0.0.1:8000 --input $1 --nb-actors $2 --nb-chunks $3 --output -
 
